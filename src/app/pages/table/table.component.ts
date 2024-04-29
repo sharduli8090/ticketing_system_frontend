9743bd5552ede3e3b280624dbee4de1e46eaa22a
Sharduli Pandey <shardulipandey8090@gmail.com>
2024-04-30 01:41:00 +0530

    sample modal created

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,13 +1,14 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AdminService } from '../../core/services/admin/admin.service';
-import { LoaderComponent } from '../loader/loader.component';
 import { AllEmployeesComponent } from '../all-employees/all-employees.component';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [NgFor, NgIf, LoaderComponent],
+  imports: [NgFor, NgIf, LoaderComponent, FormsModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css',
 })
@@ -15,7 +16,10 @@ export class TableComponent {
   @Input() data: any[] = [];
   keys: string[] = [];
   loading: boolean = false;
-  constructor(private adminService: AdminService, private getAllEmployees: AllEmployeesComponent) {}
+  constructor(
+    private adminService: AdminService,
+    private getAllEmployees: AllEmployeesComponent
+  ) {}
 
   ngOnChanges(): void {
     if (this.data.length > 0) {
@@ -32,7 +36,7 @@ export class TableComponent {
     this.loading = true;
     if (userType === 'ticket') {
       alert('ticket delete called' + ' ' + id);
-    } else { 
+    } else {
       this.adminService.deleteEmployee(id).subscribe({
         next: (response) => {
           this.loading = false;
@@ -49,8 +53,26 @@ export class TableComponent {
   }
 
   openUpdateModal(row: any): void {
-    alert('update called' + ' ' + row);
+    // alert('update called' + ' ' + row);
     // Call a function to open the update modal
     // You can pass the entire row object to the function to populate the modal fields
+    this.openModal();
+  }
+  isOpen: boolean = false;
+  inputValue: string = '';
+
+  openModal(): void {
+    this.isOpen = true;
+  }
+
+  closeModal(): void {
+    this.isOpen = false;
+  }
+
+  submitForm(): void {
+    // Perform form submission logic here
+    console.log('Submitted value:', this.inputValue);
+    // Close the modal
+    this.closeModal();
   }
 }
